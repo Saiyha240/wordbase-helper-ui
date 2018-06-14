a0c4231c5851f6107be25522aaf24a72cff65ca0
Mark Cinco <mark5cinco92@gmail.com>
2018-06-14 14:03:07 +0900

    Clean code Fix sort bug

--- js/app.js
+++ js/app.js
@@ -1,119 +1,124 @@
 var app = angular.module('WordBaserApp', ['smart-table']);
 
-app.controller('WBTableCtrl', [ '$scope', 'WBTableState', function( $scope, WBTableState ){
+app.controller('WBTableCtrl', ['$scope', 'WBTableState', function ($scope, WBTableState) {
     $scope.rowCount = 13;
     $scope.colCount = 10;
     $scope.editMode = false;
 
-    $scope.getColumns = function() {
-        return new Array( $scope.colCount );
+    $scope.getColumns = function () {
+        return new Array($scope.colCount);
     }
 
-    $scope.getRows = function() {
-        return new Array( $scope.rowCount );
+    $scope.getRows = function () {
+        return new Array($scope.rowCount);
     }
 
-    $scope.$on( 'WBTableStateChange', function(){
+    $scope.$on('WBTableStateChange', function () {
         $scope.editMode = WBTableState.isEditMode;
     });
 
 
 }]);
 
-app.controller('ButtonActionCtrl', [ '$scope', 'WBTableState', 'WBTable', function( $scope, WBTableState, WBTable ){
+app.controller('ButtonActionCtrl', ['$scope', 'WBTableState', 'WBTable', function ($scope, WBTableState, WBTable) {
     $scope.editModeButtonText = "Edit Mode";
 
-    $scope.editMode = function(){
-        if( WBTableState.toggleEditMode() ){
+    $scope.editMode = function () {
+        if (WBTableState.toggleEditMode()) {
             $scope.editModeButtonText = "Stop Edit Mode...";
-        }else{
+        } else {
             $scope.editModeButtonText = "Edit Mode";
         }
     };
 
-    $scope.resetCellState = function(){
+    $scope.resetCellState = function () {
         WBTableState.resetCellState();
     };
 
-    $scope.search = function(){
-        WBTableState.findAnswer( WBTable.getCells() );
+    $scope.search = function () {
+        WBTableState.findAnswer(WBTable.getCells());
     };
 
 }]);
 
-app.controller('HighlightCtrl', [ '$scope', 'WBTableState', function( $scope, WBTableState){
+app.controller('HighlightCtrl', ['$scope', 'WBTableState', function ($scope, WBTableState) {
     $scope.highlight = "user";
 
-    $scope.$watch('highlight', function( value ) {
-        WBTableState.setCurrentHighlight( value );
+    $scope.$watch('highlight', function (value) {
+        WBTableState.setCurrentHighlight(value);
     });
 }]);
 
-app.controller('PositionCtrl', [ '$scope', 'WBTableState', function( $scope, WBTableState){
+app.controller('PositionCtrl', ['$scope', 'WBTableState', function ($scope, WBTableState) {
     $scope.position = "top";
 
-    $scope.$watch('position', function( value ) {
-        WBTableState.setCurrentPosition( value );
+    $scope.$watch('position', function (value) {
+        WBTableState.setCurrentPosition(value);
     });
 }]);
 
-app.controller('WordListCtrl', [ '$scope', '$http', '$filter', 'WBTableState', 'Word', 'Point', function( $scope, $http, $filter, WBTableState, Word, Point ){
+app.controller('WordListCtrl', ['$scope', '$http', '$filter', 'WBTableState', 'Word', 'Point', function ($scope, $http, $filter, WBTableState, Word, Point) {
     $scope.words = [];
     $scope.displayedWords = [].concat($scope.words);
     $scope.letterFilterKey = "";
 
-    $scope.showWord = function(word){
+    $scope.showWord = function (word) {
         WBTableState.resetWordState();
 
-        angular.forEach( word.letters, function( obj, index ){
-           WBTableState.findLetter( obj );
+        angular.forEach(word.letters, function (obj, index) {
+            WBTableState.findLetter(obj);
         });
     };
 
-    $scope.$on( 'WordListFindWord', function( evt, letter){
+    $scope.$on('WordListFindWord', function (evt, letter) {
         $scope.letterFilterKey = letter;
     });
 
-    $scope.$on( 'WBTableFindAnswer', function( evt, cells){
+    $scope.$on('WBTableFindAnswer', function (evt, cells) {
         var markedMap = [],
             cellString = "";
 
-        angular.forEach( cells, function( cell ){
-            if( cell.element.hasClass( 'm-occupied' ) ){
-                markedMap.push( new Point( cell.letter.xCoor, cell.letter.yCoor ) );
+        angular.forEach(cells, function (cell) {
+            if (cell.element.hasClass('m-occupied')) {
+                markedMap.push(new Point(cell.letter.xCoor, cell.letter.yCoor));
             }
             cellString += cell.letter.letter;
         });
 
-        $http.post('http://apps-fxperiments.rhcloud.com/apps/wordbaser/crawl', angular.toJson( {rowCount:"13", colCount:"10", markedMap: markedMap, cellsString: angular.lowercase(cellString)} ) ).
-        then(function(response) {
+        $http.post('https://docuiemkgi.execute-api.ap-northeast-1.amazonaws.com/api/', angular.toJson({
+            rowCount: "13",
+            colCount: "10",
+            markedMap: markedMap,
+            cellsString: angular.lowercase(cellString)
+        })).then(function (response) {
             $scope.words = [];
-            angular.forEach( response.data, function( obj ){
-                $scope.words.push( new Word( obj.word, obj.letters ) );
+            angular.forEach(response.data, function (obj) {
+                $scope.words.push(new Word(obj.word, obj.letters));
             });
-        }, function(response) {
+            console.log($scope.words)
+        }, function (response) {
             console.log("BADS: " + response);
         });
 
     });
 
-    $scope.getters={
+    $scope.getters = {
         length: function (value) {
             return value.word.length;
         },
-        furthest: function(value){
+        furthest: function (value) {
             value.furthest;
             return value.furthest;
         }
     }
 }]);
 
-app.filter('startsWithLetter', ['WBTableState', function ( WBTableState ) {
-    return function ( items, letterFilterKey ) {
+app.filter('startsWithLetter', ['WBTableState', function (WBTableState) {
+    return function (items, letterFilterKey) {
 
-        if( WBTableState.currentHighlight == "word" && letterFilterKey != "" ){
-            return items.filter(function(word){
-                return word.letters.some(function(letter){
+        if (WBTableState.currentHighlight == "word" && letterFilterKey != "") {
+            return items.filter(function (word) {
+                return word.letters.some(function (letter) {
                     return letter.xCoor == letterFilterKey.xCoor && letter.yCoor == letterFilterKey.yCoor
                 });
             })
@@ -122,83 +127,85 @@ app.filter('startsWithLetter', ['WBTableState', function ( WBTableState ) {
     };
 }]);
 
-app.directive('cell', [ '$rootScope', 'WBTableState', 'Letter', 'WBTable', function( $rootScope, WBTableState, Letter, WBTable ) {
+app.directive('cell', ['$rootScope', 'WBTableState', 'Letter', 'WBTable', function ($rootScope, WBTableState, Letter, WBTable) {
     return {
-        restrict:'A',
-        link: function ( scope, element ) {
-            scope.letter = new Letter( "", scope.$index, scope.$parent.$index );
+        restrict: 'A',
+        link: function (scope, element) {
+            scope.letter = new Letter("", scope.$index, scope.$parent.$index);
             scope.isEditMode = false;
             scope.currentHighlight = "user";
             scope.span = element.find('span');
 
-            WBTable.addCell( { element: element, letter: scope.letter } );
+            WBTable.addCell({element: element, letter: scope.letter});
 
-            element.find('input').on('keyup', function(e){
-                if( element.next().length == 0 ){
+            element.find('input').on('keyup', function (e) {
+                if ([8, 37, 39, 46].some(code => code === e.keyCode)) return;
+
+                if (element.next().length == 0) {
                     element.parent().next().find('input')[0].focus();
-                }else{
+                } else {
                     element.next().find('input')[0].focus();
                 }
 
             });
 
-            scope.span.on('mousedown', function(){
-                if( WBTableState.isEditMode ) return;
+            scope.span.on('mousedown', function () {
+                if (WBTableState.isEditMode) return;
 
                 WBTableState.setIsMouseDown(true);
 
-                if( WBTableState.currentHighlight == "word"){
-                    $rootScope.$broadcast( 'WordListFindWord', scope.letter );
+                if (WBTableState.currentHighlight == "word") {
+                    $rootScope.$broadcast('WordListFindWord', scope.letter);
                     scope.$apply();
-                }else if( scope.currentHighlight === "user" ){
+                } else if (scope.currentHighlight === "user") {
                     element.removeClass('m-opponent').toggleClass('m-occupied');
-                }else{
+                } else {
                     element.removeClass('m-occupied').toggleClass('m-opponent');
                 }
 
                 return false;
             });
 
-            scope.span.on('mouseover', function(){
-                if( WBTableState.isEditMode ) return;
+            scope.span.on('mouseover', function () {
+                if (WBTableState.isEditMode) return;
 
-                if( WBTableState.isMouseDown ){
-                    if( scope.currentHighlight === "user" ){
+                if (WBTableState.isMouseDown) {
+                    if (scope.currentHighlight === "user") {
                         element.removeClass('m-opponent').toggleClass('m-occupied');
-                    }else{
+                    } else {
                         element.removeClass('m-occupied').toggleClass('m-opponent');
                     }
                 }
 
             });
 
-            scope.span.on('mouseup', function(){
+            scope.span.on('mouseup', function () {
                 WBTableState.setIsMouseDown(false);
             });
 
-            scope.$on('WBTableResetCellState', function(){
+            scope.$on('WBTableResetCellState', function () {
                 element.removeClass('m-occupied m-highlight m-opponent done');
             });
 
-            scope.$on('WBTableResetWordState', function(){
+            scope.$on('WBTableResetWordState', function () {
                 element.removeClass('m-highlight done');
             });
 
-            scope.$on( 'WBTableStateChange', function(){
+            scope.$on('WBTableStateChange', function () {
                 scope.isEditMode = WBTableState.isEditMode;
             });
 
-            scope.$on( 'WBTableHighlightChange', function () {
-               scope.currentHighlight = WBTableState.currentHighlight;
+            scope.$on('WBTableHighlightChange', function () {
+                scope.currentHighlight = WBTableState.currentHighlight;
             });
 
-            scope.$on( 'WBTableHighlightLetter', function( evt, letter ){
-                if( element.hasClass('done') ) return;
+            scope.$on('WBTableHighlightLetter', function (evt, letter) {
+                if (element.hasClass('done')) return;
                 //angular.equals( scope.letter, letter )
                 //scope.letter.xCoor == letter.xCoor && scope.letter.yCoor == letter.yCoor
-                if( scope.letter.xCoor == letter.xCoor && scope.letter.yCoor == letter.yCoor ){
+                if (scope.letter.xCoor == letter.xCoor && scope.letter.yCoor == letter.yCoor) {
                     element.addClass('m-highlight done');
-                }else{
+                } else {
                     element.removeClass('m-highlight');
                 }
             });
@@ -206,80 +213,80 @@ app.directive('cell', [ '$rootScope', 'WBTableState', 'Letter', 'WBTable', funct
     };
 }]);
 
-app.factory( 'WBTableState', ['$rootScope', function( $rootScope ){
+app.factory('WBTableState', ['$rootScope', function ($rootScope) {
     return {
         isEditMode: false,
         isMouseDown: false,
         currentHighlight: "user",
         currentPosition: "top",
 
-        toggleEditMode: function(){
+        toggleEditMode: function () {
             this.isEditMode = !this.isEditMode;
 
-            $rootScope.$broadcast( 'WBTableStateChange' );
+            $rootScope.$broadcast('WBTableStateChange');
 
             return this.isEditMode;
         },
 
-        resetCellState: function(){
-            $rootScope.$broadcast( 'WBTableResetCellState' );
+        resetCellState: function () {
+            $rootScope.$broadcast('WBTableResetCellState');
         },
 
-        resetWordState: function(){
-            $rootScope.$broadcast( 'WBTableResetWordState' );
+        resetWordState: function () {
+            $rootScope.$broadcast('WBTableResetWordState');
         },
 
-        setCurrentHighlight: function( value ){
+        setCurrentHighlight: function (value) {
             this.currentHighlight = value;
-            $rootScope.$broadcast( 'WBTableHighlightChange' );
+            $rootScope.$broadcast('WBTableHighlightChange');
         },
 
-        setCurrentPosition: function( value ){
+        setCurrentPosition: function (value) {
             this.currentPosition = value;
         },
 
-        setIsMouseDown: function(boolean){
+        setIsMouseDown: function (boolean) {
             this.isMouseDown = boolean;
 
             return this.isEditMode;
         },
 
-        findLetter: function( letter ){
-            $rootScope.$broadcast( 'WBTableHighlightLetter', letter );
+        findLetter: function (letter) {
+            $rootScope.$broadcast('WBTableHighlightLetter', letter);
         },
 
-        findAnswer: function( cells ){
-            $rootScope.$broadcast( 'WBTableFindAnswer', cells );
+        findAnswer: function (cells) {
+            $rootScope.$broadcast('WBTableFindAnswer', cells);
         }
     };
 }]);
 
-app.factory( 'WBTable', function(){
+app.factory('WBTable', function () {
     return {
         cells: [],
-        addCell: function( cell ){
-            this.cells.push( cell )
+        addCell: function (cell) {
+            this.cells.push(cell)
         },
-        getCells: function(){
+        getCells: function () {
             return this.cells;
         }
     };
 });
 
-app.factory( 'Word', ['Letter', 'WBTableState', function( Letter, WBTableState ){
-    return function( word, letters ){
+app.factory('Word', ['Letter', 'WBTableState', function (Letter, WBTableState) {
+    return function (word, letters) {
         var myLetters = [],
             isPositionTop = WBTableState.currentPosition == "top",
             furthest = isPositionTop ? 0 : 12;
 
-        angular.forEach( letters, function( letter ){
-            if( isPositionTop ){
-                furthest = furthest > parseInt( letter.y, 10 ) ? furthest : parseInt( letter.y, 10 );
-            }else{
-                furthest = furthest > parseInt( letter.y, 10 ) ? parseInt( letter.y, 10 ) : furthest;
+        angular.forEach(letters, function (letter) {
+            if (isPositionTop) {
+                furthest = furthest > parseInt(letter.y, 10) ? furthest : parseInt(letter.y, 10);
+            } else {
+                furthest = furthest > parseInt(letter.y, 10) ? parseInt(letter.y, 10) : furthest;
             }
 
-            myLetters.push( new Letter( letter.letter, parseInt( letter.x, 10), parseInt( letter.y, 10 ) ) )
+            myLetters.push(new Letter(letter.letter, parseInt(letter.x, 10), parseInt(letter.y, 10)))
         })
 
         return {
@@ -290,8 +297,8 @@ app.factory( 'Word', ['Letter', 'WBTableState', function( Letter, WBTableState )
     }
 }]);
 
-app.factory( 'Point', function(){
-    return function( xcoor, ycoor ){
+app.factory('Point', function () {
+    return function (xcoor, ycoor) {
         return {
             xCoor: xcoor,
             yCoor: ycoor
@@ -299,12 +306,12 @@ app.factory( 'Point', function(){
     }
 });
 
-app.factory( 'Letter', function(){
-   return function( letter, xcoor, ycoor ){
-       return {
-           letter: letter,
-           xCoor: xcoor,
-           yCoor: ycoor
-       }
-   }
+app.factory('Letter', function () {
+    return function (letter, xcoor, ycoor) {
+        return {
+            letter: letter,
+            xCoor: xcoor,
+            yCoor: ycoor
+        }
+    }
 });
